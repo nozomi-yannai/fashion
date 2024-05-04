98f647d4980c8ff85eeeb5b445dc5143a87e7a8b
“nozomi-yannai” <n.yannai.oc@gmail.com>
2024-05-04 19:41:08 +0900

    Unicorn add

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,27 @@ set :bundle_without, %w{test}.join(':')
 set :branch, 'main'
 set :rbenv_version, '3.0.6'
 set :linked_files, %w{config/secrets.yml .env}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/uploads}
+
+after 'deploy:published', 'deploy:seed'   # 9
+after 'deploy:finished', 'deploy:restart'   # 10
+
+namespace :deploy do
+  desc 'Run seed'
+  task :seed do
+    on roles(:db) do
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:seed'
+        end
+      end
+    end
+  end
+  desc 'Restart application'
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
